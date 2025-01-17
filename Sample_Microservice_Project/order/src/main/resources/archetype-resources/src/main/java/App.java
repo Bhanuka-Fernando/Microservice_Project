d88f6675a1f2c789eb57f9516f3e501534658ef9
Bhanuka-Fernando <bhanukafernando2@gmail.com>
2025-01-17 14:21:57 +0530

    created order module

---
+++ Sample_Microservice_Project/order/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $org.sample_microservice_project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
