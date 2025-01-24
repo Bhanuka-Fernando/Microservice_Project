080553de56f11cba1dfa2cb2ed20e0565e7e711c
Bhanuka-Fernando <bhanukafernando2@gmail.com>
2025-01-24 12:08:03 +0530

    corrected the variable path
    path to Path(capital)

--- Sample_Microservice_Project/order/src/main/java/com/example/order/Kafka/OrderProducer.java
+++ Sample_Microservice_Project/order/src/main/java/com/example/order/Kafka/OrderProducer.java
@@ -4,7 +4,6 @@ import com.example.base.dto.OrderEventDTO;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.Message;
